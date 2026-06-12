f33d59c05d9a39173fc31ffabf22a0437918810c
Salome98342 <salomerodriguezmoscoso@gmail.com>
2026-06-12 14:23:14 -0500

    Cambios en el back

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -176,7 +176,7 @@ if _cors_allowed_origins:
         if len(partes) == 2:
             # Tomamos la parte del dominio y cortamos si hay barras adicionales (ej: /admin)
             dominio_limpio = partes[1].split('/')[0]
-            CORS_ALLOWED_ORIGINS.append(f"{partes[0]}://{dominio_limpio}")
+            CORS_ALLOWED_ORIGINS.append(f"{partes[0]}://{360-digital.vercel.app}")
         else:
             # Fallback en caso de que el formato sea diferente
             CORS_ALLOWED_ORIGINS.append(origen.rstrip('/'))
