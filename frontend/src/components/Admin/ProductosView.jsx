4cc2a2a3dd5881e32c949896b75e8be5ab6b606f
Salomé <salomerodriguezmoscoso@gmail.com>
2026-06-19 16:23:09 -0500

    Cambios en el Css del Admin

--- frontend/src/components/Admin/ProductosView.jsx
+++ frontend/src/components/Admin/ProductosView.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from 'react';
 import { toast } from 'react-hot-toast';
 import { getCookie } from '../../src/utils/helpers';
-import styles from './AdminDashboard.module.css';
+import styles from '../../pages/AdminDashboard.module.css';
 
 export default function ProductosView() {
   const [productos, setProductos] = useState([]);
